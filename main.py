8a08aa0687c2e5db056111d142497913d5cdb0b4
Andrew <andrewqp03@gmail.com>
2025-03-01 01:16:38 -0600

    working on preprocessing

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import time
 from datetime import datetime
 from random import randint
+import re
+from emoji import demojize
 
 load_dotenv();
 
@@ -20,14 +22,21 @@ async def twitterLogin():
 
 # Get Tweets
 async def getTwitterPosts():
-    tweets = await client.search_tweet("Elon", product='Top')
+    tweets = await client.search_tweet("(Elon OR Musk) min_faves:10 lang:en -filter:links filter:replies", product='Latest', count=10)
     tweetCount = 0
     for tweet in tweets:
         tweetCount += 1
-        tweetData = [tweetCount, tweet.user.name, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favorite_count, tweet.reply_count]
-        print(tweetData)
-        break;
+        #tweetData = [tweetCount, preprocessing_tweet(tweet.text), tweet.reply_count, tweet.view_count, tweet.favorite_count, tweet.retweet_count, tweet.created_at]
+        #print(tweetData)
+        print([tweetCount, tweet.text])
+        print([tweetCount, preprocessing_tweet(tweet.text)])
 
+def preprocessing_tweet(text):
+    text = demojize(text); # Convert emoji to text
+    text = re.sub(r'@\w+', '[MENTION]', text) # Remove @ to another user
+    text = re.sub(r'#(\w+)', r'\1', text) # Remove # but keeps text
+    text = text.strip() # Remove extra spaces
+    return text
 
 
 client.load_cookies('cookies.json')
