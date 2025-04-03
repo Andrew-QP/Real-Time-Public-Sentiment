85c65aa266673383c4d303126211fad36b418a37
Andrew <andrewqp03@gmail.com>
2025-04-03 15:13:59 -0500

    Combining finance data and tweets work. Real test tomorrow by automatically executing every 10 mins.

--- main.py
+++ main.py
@@ -35,106 +35,104 @@ cursor.execute('''
         high DECIMAL(10, 2) NOT NULL, 
         low DECIMAL(10, 2) NOT NULL, 
         close DECIMAL(10, 2) NOT NULL, 
-        volume INTEGER NOT NULL,
+        volume INTEGER NOT NULL
     )
 ''')
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_tweet_id ON tweets (id)')
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_created_date ON tweets (createdDate)')
 conn.commit()
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS stockPrice (
-#         time TEXT PRIMARY KEY,
-#         open DECIMAL(10, 2) NOT NULL,
-#         high DECIMAL(10, 2) NOT NULL,
-#         low DECIMAL(10, 2) NOT NULL,
-#         close DECIMAL(10, 2) NOT NULL,
-#         volume INTEGER NOT NULL,
-#         volumeWeighed DECIMAL(10, 2) NOT NULL,
-#         transactions INTEGER NOT NULL
-#     )
-# ''')
-# cursor.execute('CREATE INDEX IF NOT EXISTS idx_stock_prices_time ON stockPrice(time)')
-# conn.commit()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS stockPrice (
+        time TEXT PRIMARY KEY,
+        open DECIMAL(10, 2) NOT NULL,
+        high DECIMAL(10, 2) NOT NULL,
+        low DECIMAL(10, 2) NOT NULL,
+        close DECIMAL(10, 2) NOT NULL,
+        volume INTEGER NOT NULL
+    )
+''')
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_stock_prices_time ON stockPrice(time)')
+conn.commit()
 
-# Task functions
-def collect_tweet_data():
-    print("Collecting tweet data...")
+# # Task functions
+# def collect_tweet_data():
+#     print("Collecting tweet data...")
 
-def collect_finance_data():
-    print("Collecting finance data...")
+# def collect_finance_data():
+#     print("Collecting finance data...")
 
-def make_predictions():
-    print("Making predictions...")
+# def make_predictions():
+#     print("Making predictions...")
 
-def update_graphs():
-    print("Updating graphs...")
+# def update_graphs():
+#     print("Updating graphs...")
 
-# Global flags to track task completion
-tweet_data_collected = False
-finance_data_collected = False
-lock = threading.Lock()
+# # Global flags to track task completion
+# tweet_data_collected = False
+# finance_data_collected = False
+# lock = threading.Lock()
 
-# Task completion listener
-def task_listener(event):
-    global tweet_data_collected, finance_data_collected
+# # Task completion listener
+# def task_listener(event):
+#     global tweet_data_collected, finance_data_collected
 
-    # Listen for completion of tasks
-    with lock:
-        if event.job_id == 'collect_tweets' and event.exception is None:
-            tweet_data_collected = True
-            print("Tweet data collection completed.")
+#     # Listen for completion of tasks
+#     with lock:
+#         if event.job_id == 'collect_tweets' and event.exception is None:
+#             tweet_data_collected = True
+#             print("Tweet data collection completed.")
 
-        elif event.job_id == 'collect_finance' and event.exception is None:
-            finance_data_collected = True
-            print("Finance data collection completed.")
+#         elif event.job_id == 'collect_finance' and event.exception is None:
+#             finance_data_collected = True
+#             print("Finance data collection completed.")
 
-        # Trigger predictions only if both data collection tasks are completed
-        print(f"{tweet_data_collected} | {finance_data_collected}")
+#         # Trigger predictions only if both data collection tasks are completed
+#         print(f"{tweet_data_collected} | {finance_data_collected}")
         
-        # Trigger predictions and graph updates only once after both are done
-        if tweet_data_collected and finance_data_collected:
-            # Ensure tasks are triggered only once
-            print("Both data collections completed. Running predictions and updating graphs.")
+#         # Trigger predictions and graph updates only once after both are done
+#         if tweet_data_collected and finance_data_collected:
+#             # Ensure tasks are triggered only once
+#             print("Both data collections completed. Running predictions and updating graphs.")
             
-            time.sleep(5)
-            make_predictions()
-            update_graphs()
-            print("--------------")
+#             time.sleep(5)
+#             make_predictions()
+#             update_graphs()
+#             print("--------------")
             
-            # Reset the flags after triggering the tasks
-            tweet_data_collected = False
-            finance_data_collected = False
+#             # Reset the flags after triggering the tasks
+#             tweet_data_collected = False
+#             finance_data_collected = False
 
 
-# Initialize BackgroundScheduler
-scheduler = BackgroundScheduler()
+# # Initialize BackgroundScheduler
+# scheduler = BackgroundScheduler()
 
-# Add jobs to scheduler (set specific times for tweet and finance data collection)
-scheduler.add_job(collect_tweet_data, 'cron', minute='2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58', id='collect_tweets')
-scheduler.add_job(collect_finance_data, 'cron', minute='2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58', id='collect_finance')
+# # Add jobs to scheduler (set specific times for tweet and finance data collection)
+# scheduler.add_job(collect_tweet_data, 'cron', minute='2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58', id='collect_tweets')
+# scheduler.add_job(collect_finance_data, 'cron', minute='2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58', id='collect_finance')
 
-# Add listener for task completion
-scheduler.add_listener(task_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+# # Add listener for task completion
+# scheduler.add_listener(task_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
 
-# Start the scheduler
-# driver = tws.login()
-# driver.get("https://x.com/search?q=%24TSLA%20lang%3Aen%20-filter%3Alinks&f=live&src=typed_query")
-print("Starting...")
-scheduler.start()
-
-
-
+# # Start the scheduler
+# # driver = tws.login()
+# # driver.get("https://x.com/search?q=%24TSLA%20lang%3Aen%20-filter%3Alinks&f=live&src=typed_query")
+# print("Starting...")
+# scheduler.start()
 
 
 
 
-# To keep the program running
-try:
-    while True:
-        time.sleep(1)  # Keeps the program running, allowing scheduler to run in background
-except (KeyboardInterrupt, SystemExit):
-    # Shut down the scheduler gracefully when exiting
-    scheduler.shutdown()
+
+
+
+# # To keep the program running
+# try:
+#     while True:
+#         time.sleep(1)  # Keeps the program running, allowing scheduler to run in background
+# except (KeyboardInterrupt, SystemExit):
+#     # Shut down the scheduler gracefully when exiting
+#     scheduler.shutdown()
 
 
 
